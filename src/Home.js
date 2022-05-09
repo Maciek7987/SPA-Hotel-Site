0bf626798724700ea52c2dbcb3c263783b48b69d
Maciej Chojna <maciekchojnaofficial@gmail.com>
2022-05-09 14:06:32 +0200

    add transparent background to disable scroll

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import "./style/Home.scss";
 
 //try import library to scroll
 import HorizontalScroll from "react-scroll-horizontal";
+import { Component, createRef } from "react";
 
 const handelToggle = (e, link) => {
   if (link) {
@@ -17,21 +18,47 @@ const handelToggle = (e, link) => {
   }
 };
 
-const Home = () => {
-  return (
-    <>
-      <section className="home-page">
-        <HorizontalScroll
-          pageLock={true}
-          config={{ stiffness: 28, damping: 10 }}
-        >
-          <Main/>
-        </HorizontalScroll>
-        <Navigation />
-        <Footer handelToggle={handelToggle} />
-      </section>
-    </>
-  );
-};
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.homePage = createRef();
+    this.pauseScrollElement = createRef();
+  }
+
+  componentDidMount() {
+    const target = this.homePage.current.firstChild.firstChild;
+    const style = window.getComputedStyle(target);
+    let matrix;
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach(() => {
+        matrix = new DOMMatrix(style.transform);
+        console.log(matrix.m41);
+      });
+    });
+
+    observer.observe(target, { attributes: true, attributeFilter: ["style"] });
+  }
+
+  render() {
+    return (
+      <>
+        <div ref={this.pauseScrollElement} className="pauseScrollElement"></div>
+        <section ref={this.homePage} className="home-page">
+          <HorizontalScroll
+            pageLock={true}
+            reverseScroll={true}
+            className={"horizontal-scroll"}
+            config={{ stiffness: 28, damping: 10 }}
+          >
+            <Main />
+          </HorizontalScroll>
+          <Navigation />
+          <Footer handelToggle={handelToggle} />
+        </section>
+      </>
+    );
+  }
+}
 
 export default Home;
